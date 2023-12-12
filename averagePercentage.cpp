065c3de001e88f625144419886df959d514bf27a
triyankk <triyankk@gmail.com>
2023-12-12 21:44:34 +0530

    tested

--- averagePercentage.cpp
+++ averagePercentage.cpp
@@ -1,10 +1,11 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 // Function prototype
 void calculateAverageAndPercentage(int marks1, int marks2, int marks3, double &average, double &percentage);
 
-int main() {
+int main()
+{
     // Input marks from the user
     int subject1, subject2, subject3;
     cout << "Enter marks for Subject 1: ";
@@ -17,16 +18,15 @@ int main() {
     double avg, percent;
     // Call the function using call by value and call by reference
     calculateAverageAndPercentage(subject1, subject2, subject3, avg, percent);
-
     // Display the results
     cout << "Average marks: " << avg << endl;
     cout << "Percentage: " << percent << "%" << endl;
-
     return 0;
 }
 
 // Function to calculate average and percentage using call by reference
-void calculateAverageAndPercentage(int marks1, int marks2, int marks3, double &average, double &percentage) {
+void calculateAverageAndPercentage(int marks1, int marks2, int marks3, double &average, double &percentage)
+{
     // Calculate average
     average = (marks1 + marks2 + marks3) / 3.0;
 
